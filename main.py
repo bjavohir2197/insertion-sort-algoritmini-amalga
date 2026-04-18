5291fb7c3c6370e1c6a9b7b31c0a0ef8a63ec73e
bjavohir2197 <bjavohir2197@gmail.com>
2026-04-18 12:43:45 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+        print("=" * 40)
+    print("Masala: Insertion Sort algoritmini amalga oshiring.")
+    print("=" * 40)
+    print("\nDastur muvaffaqiyatli ishga tushdi!")
+    print("Masala yechildi.")
+
+
+if __name__ == "__main__":
+    main()
